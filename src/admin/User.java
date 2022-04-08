160feab6e784ca133b3bae00a23fc754f9821f26
Hamza-R <hamza.rehawi@gmail.com>
2022-04-08 13:21:44 +0100

    User Class Getters/Setters

--- src/admin/User.java
+++ src/admin/User.java
@@ -1,4 +1,27 @@
 package admin;
 
 public class User {
+
+    public String getAccountHolder() { return accountHolder; }
+
+    public String getAccountType() { return accountType; }
+
+    public String getUsername() { return username; }
+
+    public String getPassword() { return password; }
+
+    public float getHourlyRate() { return hourlyRate; }
+
+    public void setAccountHolder(String accountHolder) { this.accountHolder = accountHolder; }
+
+    public void setAccountType(String accountType) { this.accountType = accountType; }
+
+    public void setUsername(String username) { this.username = username; }
+
+    public void setPassword(String password) { this.password = password; }
+
+    public void setHourlyRate(float hourlyRate) { this.hourlyRate = hourlyRate; }
+
+    private String accountHolder,accountType,username,password;
+    private float hourlyRate;
 }
